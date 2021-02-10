bd7c2cda94af92b0323cfb62fb8eb2753e4910fb
Massimo Boscaro <MoreGrudge@gmail.com>
2021-02-10 21:07:43 +0100

    calcolatrice con funzioni

--- client.py
+++ client.py
@@ -11,41 +11,58 @@ print(type(output_string))
 print(output_string)
 
 import socket
+import sys
 
 SERVER_ADDRESS = '127.0.0.1'
 SERVER_PORT = 22224
 
-sock_service = socket.socket()
+#la funzione riceve la socket connessa al server e la utilizza per rchiedere il servizio
+def invia_comandi(sock_service): 
+    while True:
+        try:
+            dati = input("Inserisci i dati da inviare (0 per terminare la connessione): ")
+        except EOFError:
+            print("\nOkay. Exit")
+            break
+                
+        if not dati:
+            print("Non puoi inviare una stringa vuota!")
+            continue
 
-sock_service.connect((SERVER_ADDRESS, SERVER_PORT))
+        if dati == '0':
+            print("Chiudo la connessione con il server!")
+            break
+        
+        dati = dati.encode()
 
-print("Connesso a " + str((SERVER_ADDRESS, SERVER_PORT)))
-while True:
-    try:
-        dati = input("Inserisci i dati da inviare (0 per terminare la connessione): ")
-    except EOFError:
-        print("\nOkay. Exit")
-        break
-    if not dati:
-        print("Non puoi inviare una stringa vuota!")
-        continue
-    if dati == '0':
-        print("Chiudo la connessione con il server!")
-        break
+        sock_service.send(dati)
+
+        dati = sock_service.recv(2048)
+
+        if not dati:
+            print("Server non risponde. Exit")
+            break
+        
+        dati = dati.decode()
+
+        print("Ricevuto dal server:")
+        print(dati + '\n')
+
+        
+
+        sock_service.close()
+        
+#la funzione crea una socket (s) per la connessione con il server e la passa alla funzione invia_comandi(s)
+def connessione_server(address,port):
+    sock_service = socket.socket()  
+    sock_service.connect((SERVER_ADDRESS, SERVER_PORT))
+    print("Connesso a " + str((SERVER_ADDRESS, SERVER_PORT)))
+    invia_comandi(sock_service)
     
-    dati = dati.encode()
 
-    sock_service.send(dati)
+if __name__ == '__main__':
+    connessione_server(SERVER_ADDRESS,SERVER_PORT)
+# if __name__ == '__main__': consente al nostro codice di capire se stia venendo eseguito
+# uno script a se stante, o se invece è stato richiamato come modulo da un qualche 
+#programma per usare una o più delle sue varie funzioni e classi
 
-    dati = sock_service.recv(2048)
-
-    if not dati:
-        print("Server non risponde. Exit")
-        break
-    
-    dati = dati.decode()
-
-    print("Ricevuto dal server:")
-    print(dati + '\n')
-
-sock_service.close()
--- server.py
+++ server.py
@@ -7,58 +7,71 @@ SERVER_ADDRESS = '127.0.0.1'
 
 SERVER_PORT = 22224
 
-sock_listen = socket.socket()
+#la funzione avvia_server crea un endpoint di ascolto dal quale accettare connessioni
+#in entrata, la socket di ascolto viene passata alla funzione ricevi_comandi la quale
+#accetta richieste di connessione e per ognuna crea una socket per i dati da cui
+#ricevere le richieste e inviare le risposte.
 
-sock_listen.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-sock_listen.bind((SERVER_ADDRESS, SERVER_PORT))
-
-sock_listen.listen(5)
-
-print("Server in ascolto su %s." % str((SERVER_ADDRESS, SERVER_PORT)))
-
-
-while True:
-    sock_service, addr_client = sock_listen.accept()
-    print("\nConnessione ricevuta da " + str(addr_client))
-    print("\nAspetto di ricevere i dati ")
-    contConn=0
+def ricevi_comandi(sock_listen):
     while True:
-        dati = sock_service.recv(2048)
-        contConn+=1
-        if not dati:
-            print("Fine dati dal client. Reset")
-            break
-        
-        dati = dati.decode()
-        print("Ricevuto: '%s'" % dati)
-        if dati=='0':
-            print("Chiudo la connessione con " + str(addr_client))
-            break
-
-        ris=0
-        dati=dati.split(';')
-        if dati[0]=='piu':
-            ris=int(dati[1]) + int(dati[2])
-        elif dati[0]=='meno':
-            ris=int(dati[1]) - int(dati[2])
-        elif dati[0]=='per':
-            ris=int(dati[1]) * int(dati[2])
-        elif dati[0]=='diviso':
-            ris=int(dati[1]) / int(dati[2])
-
-        ris=str(ris)
-        
-
+        sock_service, addr_client = sock_listen.accept()
+        print("\nConnessione ricevuta da " + str(addr_client))
+        print("\nAspetto di ricevere i dati ")
+        contConn=0
+        while True:
+            dati = sock_service.recv(2048)
+            contConn+=1
+            if not dati:
+                print("Fine dati dal client. Reset")
+                break
             
-        
+            dati = dati.decode()
+            print("Ricevuto: '%s'" % dati)
+            if dati=='0':
+                print("Chiudo la connessione con " + str(addr_client))
+                break
+
+            ris=0
+            dati=dati.split(';')
+            if dati[0]=='piu':
+                ris=int(dati[1]) + int(dati[2])
+            elif dati[0]=='meno':
+                ris=int(dati[1]) - int(dati[2])
+            elif dati[0]=='per':
+                ris=int(dati[1]) * int(dati[2])
+            elif dati[0]=='diviso':
+                ris=int(dati[1]) / int(dati[2])
+
+            ris=str(ris)
+            
+
+                
+            
 
 
 
-        dati = "Risposta a : " + str(addr_client) + ". Il valore del contatore è : " + str(ris)
+            dati = "Risposta a : " + str(addr_client) + ". Il valore del contatore è : " + str(ris)
 
-        dati = dati.encode()
+            dati = dati.encode()
 
-        sock_service.send(dati)
+            sock_service.send(dati)
 
-    sock_service.close()
+            sock_service.close()   
+    
+
+def avvia_server(indirizzo,porta):
+    sock_listen = socket.socket()
+
+    sock_listen.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    sock_listen.bind((SERVER_ADDRESS, SERVER_PORT))
+
+    sock_listen.listen(5)
+    print("Server in ascolto su %s." % str((SERVER_ADDRESS, SERVER_PORT)))
+    ricevi_comandi(sock_listen)
+
+if __name__ == '__main__':
+    avvia_server(SERVER_ADDRESS,SERVER_PORT)
+
+
+
